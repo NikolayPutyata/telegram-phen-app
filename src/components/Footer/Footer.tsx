580f678cf029af12a76ba55d28edfbd0c6340dd7
NikolayPutyata <93452811+NikolayPutyata@users.noreply.github.com>
2025-02-01 16:50:57 +0200

    upd

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   const location = useLocation();
 
   return (
-    <div className="dock bg-neutral text-neutral-content rounded-tl-4xl rounded-tr-4xl">
+    <div className="dock bg-neutral text-neutral-content rounded-tl-4xl rounded-tr-4xl pb-safe-bottom">
       <Link to={'/'} className={location.pathname === '/' ? 'dock-active' : ''}>
         <img src="/assets/home.svg" alt="" width={24}/>
         <span className="dock-label">Home</span>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,5 +2,12 @@ import daisyui from 'daisyui';
 
 export default {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      padding: {
+        'safe-bottom': 'env(safe-area-inset-bottom)',
+      },
+    },
+  },
   plugins: [daisyui],
 };
