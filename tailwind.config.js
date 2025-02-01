25c53d5bd2f825dbed1861e293463a5d9ee799f9
NikolayPutyata <93452811+NikolayPutyata@users.noreply.github.com>
2025-02-01 16:52:34 +0200

    upd

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,5 @@ import daisyui from 'daisyui';
 
 export default {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {
-      padding: {
-        'safe-bottom': 'env(safe-area-inset-bottom)',
-      },
-    },
-  },
   plugins: [daisyui],
 };
