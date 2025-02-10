cbae645db4929b6e7553545c9ab6c540128697d2
NikolayPutyata <93452811+NikolayPutyata@users.noreply.github.com>
2025-02-10 11:16:27 +0200

    some presale upd

--- src/components/PresaleBlock/PresaleBlock.tsx
+++ src/components/PresaleBlock/PresaleBlock.tsx
@@ -6,14 +6,17 @@ const PresaleBlock = () => {
       <h2 className={`${s.font} text-zinc-300 ml-4 my-4`}>
         Presale 🚀
       </h2>
+      
       <div className="flex flex-col justify-center px-3 my-8">
         <div className="flex flex-col px-6 my-4">
           <div className="flex justify-between ">
             <p className={`${s.font} text-zinc-400 `}>Presale price</p>
+            
             <p className={`${s.font} text-zinc-400  tracking-wider`}>
               $0.0015
             </p>
           </div>
+          <div className='mt-4 mb-3 flex justify-center items-center'><input type="text" placeholder="PHEN" className="input input-primary rounded-4xl py-4" /></div>
         </div>
         <button className="btn btn-primary w-56 rounded-4xl self-center">
           Buy
