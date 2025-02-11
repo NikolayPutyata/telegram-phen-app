dec04fbb5aa02b82888b8666e69b800aef4c181c
nykyforuk_vasyl <nikiforuk.vasya@gmail.com>
2025-02-11 18:28:23 +0200

    upd

--- src/components/Skins/Skins.tsx
+++ src/components/Skins/Skins.tsx
@@ -1,10 +1,83 @@
+import s from '/src/App.module.css';
+// import { useSelector } from 'react-redux';
 
 const Skins = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  // const skins = useSelector(selectSkinsId);
 
-export default Skins
+  return (
+    <div className={`${s.font} flex flex-col px-7 mb-5 tracking-wider`}>
+      <ul className="flex flex-col gap-6">
+        <h2 className="text-zinc-300 my-3">Active specials 👑</h2>
+
+        {/* {special.map((skin) => ( */}
+        <li className="flex pl-2 ml-2 gap-10">
+          <div className="flex flex-col justify-center w-32 h-32 overflow-hidden rounded-3xl">
+            <img src="/assets/shuttle-2.webp" alt="standart avatar" />
+          </div>
+
+          <div className="flex justify-start flex-col">
+            <h3 className="text-zinc-300 mb-2 text-xl break-words">
+              Misterium
+            </h3>
+            <p className="text-zinc-400 text-base mb-2">X3</p>
+          </div>
+        </li>
+        {/* ))} */}
+
+        <li className="flex pl-2 ml-2 gap-10">
+          <div className="flex flex-col justify-center w-32 h-32 overflow-hidden rounded-3xl">
+            <img src="/assets/shuttle-2.webp" alt="standart avatar" />
+          </div>
+
+          <div className="flex justify-start flex-col">
+            <h3 className="text-zinc-300 mb-2 text-xl break-words">
+              Misterium
+            </h3>
+            <p className="text-zinc-400 text-base mb-2">X3</p>
+          </div>
+        </li>
+
+        <li className="flex pl-2 ml-2 gap-10">
+          <div className="flex flex-col justify-center w-32 h-32 overflow-hidden rounded-3xl">
+            <img src="/assets/shuttle-2.webp" alt="standart avatar" />
+          </div>
+
+          <div className="flex justify-start flex-col">
+            <h3 className="text-zinc-300 mb-2 text-xl break-words">
+              Misterium
+            </h3>
+            <p className="text-zinc-400 text-base mb-2">X3</p>
+          </div>
+        </li>
+
+        <li className="flex pl-2 ml-2 gap-10">
+          <div className="flex flex-col justify-center w-32 h-32 overflow-hidden rounded-3xl">
+            <img src="/assets/shuttle-2.webp" alt="standart avatar" />
+          </div>
+
+          <div className="flex justify-start flex-col">
+            <h3 className="text-zinc-300 mb-2 text-xl break-words">
+              Misterium
+            </h3>
+            <p className="text-zinc-400 text-base mb-2">X3</p>
+          </div>
+        </li>
+
+        <li className="flex pl-2 ml-2 gap-10">
+          <div className="flex flex-col justify-center w-32 h-32 overflow-hidden rounded-3xl">
+            <img src="/assets/shuttle-2.webp" alt="standart avatar" />
+          </div>
+
+          <div className="flex justify-start flex-col">
+            <h3 className="text-zinc-300 mb-2 text-xl break-words">
+              Misterium
+            </h3>
+            <p className="text-zinc-400 text-base mb-2">X3</p>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Skins;
--- src/components/SkinsModal/SkinsModal.tsx
+++ src/components/SkinsModal/SkinsModal.tsx
@@ -5,11 +5,9 @@ const SkinsModal = () => {
   // const skins = useSelector(selectSkinsId);
 
   return (
-    <div>
-      <ul className="flex flex-col  gap-4">
-        <h2 className={`${s.font} text-zinc-300 tracking-wider my-3`}>
-          Active skins 👑
-        </h2>
+    <>
+      <ul className={`${s.font} flex flex-col tracking-wider gap-4`}>
+        <h2 className="text-zinc-300 my-3">Active skins 👑</h2>
 
         {/* {skins.map((skin) => ( */}
         <li className="flex pl-2  gap-10">
@@ -18,21 +16,13 @@ const SkinsModal = () => {
           </div>
 
           <div className="flex justify-start flex-col">
-            <h3
-              className={`${s.font} text-zinc-300 mb-2 break-words tracking-wider`}
-            >
-              Misterium
-            </h3>
-            <p
-              className={`${s.font} text-zinc-400 text-sm mb-2 tracking-wider`}
-            >
-              X3
-            </p>
+            <h3 className="text-zinc-300 mb-2 break-words">Misterium</h3>
+            <p className="text-zinc-400 text-sm mb-2">X3</p>
           </div>
         </li>
         {/* ))} */}
       </ul>
-    </div>
+    </>
   );
 };
 
