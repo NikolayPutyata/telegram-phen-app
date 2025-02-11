d9a88fb515aabf465aaf9662c80e468fa9c84236
NikolayPutyata <93452811+NikolayPutyata@users.noreply.github.com>
2025-02-11 11:29:07 +0200

    add addition boosts routing

--- src/Pages/Leaderbords/Leaderbords.tsx
+++ src/Pages/Leaderbords/Leaderbords.tsx
@@ -1,35 +1,48 @@
-import BoostsItem from './BoostsItem';
-import s from '/src/App.module.css';
+import { NavLink, Outlet, useLocation } from "react-router-dom";
 
-const Leaderboards = () => {
-  const boosts = [
-    { imgSrc: "/assets/engine.webp", title: "V3 Engine", desc: "2x Farming Boost", price: "0.02 TON" },
-    { imgSrc: "/assets/commandor.webp", title: "Co-pilot", desc: "4x Farming Boost", price: "0.04 TON" },
-    { imgSrc: "/assets/rangers.webp", title: "Ranger Team", desc: "8x Farming Boost", price: "0.08 TON" },
-  ];
-  const specialBoosts = [
-  { imgSrc: "/assets/shuttle-2.webp", title: "Flagship", desc: "20x Farming Boost", price: "0.20 TON" },
-    { imgSrc: "/assets/shuttle-1.webp", title: "Space Cruiser", desc: "15x Farming Boost", price: "0.15 TON" },
-    
-    { imgSrc: "/assets/shuttle-3.webp", title: "Shuttle", desc: "10x Farming Boost", price: "0.10 TON" },
-  ];
+
+const Leaderbords = () => {
+
+  const location = useLocation();
 
   return (
-    <div>
-      <h2 className={`${s.font} text-zinc-300 ml-8 my-3`}>Boosts ⭐</h2>
-      <ul className="flex flex-col gap-6">
-        {boosts.map((product, index) => (
-          <BoostsItem key={index} {...product} />
-        ))}
-      </ul>
-      <h2 className={`${s.font} text-zinc-300 ml-8 my-6 items-center`}>Special Boosts 👑</h2>
-      <ul className="flex flex-col gap-6 mb-32">
-        {specialBoosts.map((product, index) => (
-          <BoostsItem key={index} {...product} />
-        ))}
-      </ul>
-    </div>
-  );
-};
+   <>
+      <div className="my-3">
+        <div role="tablist" className="tabs tabs-border">
+          <NavLink
+            to="/boosts"
+            role="tab"
+            className={`tab ${
+              location.pathname === "/boosts" ? "tab-active" : ""
+            }`}
+          >
+            Boosts
+          </NavLink>
+          <NavLink
+            to="/boosts/skins"
+            role="tab"
+            className={`tab ${
+              location.pathname === "/boosts/skins" ? "tab-active" : ""
+            }`}
+          >
+            Skins
+          </NavLink>
+          <NavLink
+            to="/boosts/special"
+            role="tab"
+            className={`tab ${
+              location.pathname === "/boosts/special" ? "tab-active" : ""
+            }`}
+          >
+            Special
+          </NavLink>
+        </div>
+      </div>
+  
+      <Outlet />
+    </>
+  )
+}
 
-export default Leaderboards;
+export default Leaderbords
+
---
+++ src/components/Boosts/Boosts.tsx
@@ -0,0 +1,39 @@
+import BoostsItem from "../../Pages/Leaderbords/BoostsItem";
+import s from '/src/App.module.css';
+
+const Boosts = () => {
+
+    const boosts = [
+    { imgSrc: "/assets/engine.webp", title: "V3 Engine", desc: "2x Farming Boost", price: "0.02 TON" },
+    { imgSrc: "/assets/commandor.webp", title: "Co-pilot", desc: "4x Farming Boost", price: "0.04 TON" },
+    { imgSrc: "/assets/rangers.webp", title: "Ranger Team", desc: "8x Farming Boost", price: "0.08 TON" },
+  ];
+  const specialBoosts = [
+  { imgSrc: "/assets/shuttle-2.webp", title: "Flagship", desc: "20x Farming Boost", price: "0.20 TON" },
+    { imgSrc: "/assets/shuttle-1.webp", title: "Space Cruiser", desc: "15x Farming Boost", price: "0.15 TON" },
+    
+    { imgSrc: "/assets/shuttle-3.webp", title: "Shuttle", desc: "10x Farming Boost", price: "0.10 TON" },
+  ];
+
+    
+  return (
+    <div>
+      <h2 className={`${s.font} text-zinc-300 ml-8 my-3`}>Boosts ⭐</h2>
+      <ul className="flex flex-col gap-6">
+        {boosts.map((product, index) => (
+          <BoostsItem key={index} {...product} />
+        ))}
+      </ul>
+      <h2 className={`${s.font} text-zinc-300 ml-8 my-6 items-center`}>Special Boosts 👑</h2>
+      <ul className="flex flex-col gap-6 mb-32">
+        {specialBoosts.map((product, index) => (
+          <BoostsItem key={index} {...product} />
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default Boosts
+
+
