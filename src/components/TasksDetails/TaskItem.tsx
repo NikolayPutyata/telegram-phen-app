9f7138c85198bfa07f08b25dadb6d5143e8cf725
nykyforuk_vasyl <nikiforuk.vasya@gmail.com>
2025-02-14 15:27:51 +0200

    TaskItem svg

--- src/components/TasksDetails/TaskItem.tsx
+++ src/components/TasksDetails/TaskItem.tsx
@@ -7,11 +7,16 @@ interface TaskItemProps {
   completed: boolean;
 }
 
-const TaskItem: React.FC<TaskItemProps> = ({ src, title, bonus, completed }) => {
+const TaskItem: React.FC<TaskItemProps> = ({
+  src,
+  title,
+  bonus,
+  completed,
+}) => {
   return (
     <li className="flex text-center justify-between px-4 items-center">
-      <div className="flex gap-1">
-        <img src={src} width={16} height={16}/>
+      <div className="flex items-center gap-1">
+        <img src={src} className="mr-1 w-7 h-7" />
         <div className="flex flex-col justify-start">
           <p className={` ${s.font} flex justify-start text-sm`}>{title}</p>
           <span className="flex items-center gap-1.5">
@@ -20,7 +25,9 @@ const TaskItem: React.FC<TaskItemProps> = ({ src, title, bonus, completed }) => 
           </span>
         </div>
       </div>
-      {completed === false ? <button className="btn btn-outline rounded-3xl px-8">Go</button> : null}
+      {completed === false ? (
+        <button className="btn btn-outline rounded-3xl px-7">Go</button>
+      ) : null}
     </li>
   );
 };
