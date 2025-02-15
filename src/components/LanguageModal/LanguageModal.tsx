ccf7a428754dec4fa5e3bff6ef2f5441ce21f49f
nykyforuk_vasyl <nikiforuk.vasya@gmail.com>
2025-02-15 20:00:56 +0200

    language upd icon

--- src/components/LanguageModal/LanguageModal.tsx
+++ src/components/LanguageModal/LanguageModal.tsx
@@ -9,34 +9,58 @@ const LanguageModal: React.FC<LanguageModalProps> = ({ onClose }) => {
   const { i18n } = useTranslation();
 
   const languages = [
-    { code: 'en', name: 'English' },
-    { code: 'es', name: 'Español' },
-    { code: 'de', name: 'Deutsch' },
-    { code: 'fr', name: 'Français' },
-    { code: 'it', name: 'Italiano' },
-    { code: 'pl', name: 'Polski' },
+    {
+      code: 'en',
+      name: 'English',
+      src: '/assets/english.svg',
+    },
+    {
+      code: 'es',
+      name: 'Español',
+      src: '/assets/espanol.svg',
+    },
+    {
+      code: 'de',
+      name: 'Deutsch',
+      src: '/assets/deutsch.svg',
+    },
+    {
+      code: 'fr',
+      name: 'Français',
+      src: '/assets/francais.svg',
+    },
+    {
+      code: 'it',
+      name: 'Italiano',
+      src: '/assets/italiano.svg',
+    },
+    { code: 'pl', name: 'Polski', src: '/assets/polski.svg' },
   ];
   const handleLanguageChange = (code: string) => {
     i18n.changeLanguage(code);
-    onClose(); // Закриваємо модалку
+    onClose();
   };
 
   return (
     <div className="p-6 flex-col flex items-center">
-      <h2 className={`${s.font} text-zinc-300 text-xl tracking-wider mb-3`}>
-        Select Language
-      </h2>
+      <div className="flex justify-center gap-2">
+        <h2 className={`${s.font} text-zinc-300 text-xl tracking-wider mb-3`}>
+          Select Language
+        </h2>
+        <img src="/public/assets/language.svg" className="w-6 h-6" />
+      </div>
       <ul>
-        {languages.map(({ code, name }) => (
+        {languages.map(({ code, name, src }) => (
           <li
             key={code}
-            className={`p-2 ${s.font} tracking-wider   text-lg ${
+            className={`p-2 ${s.font} tracking-wider flex gap-2 text-lg ${
               i18n.language === code
                 ? `mb-2 text-zinc-100 tracking-wider`
                 : 'text-zinc-400'
             }`}
             onClick={() => handleLanguageChange(code)}
           >
+            <img src={src} className="mr-1 w-7 h-7" />
             {name}
           </li>
         ))}
