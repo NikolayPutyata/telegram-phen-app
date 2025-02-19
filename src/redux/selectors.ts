d272ed8228b13b7c8d51d278f4f02d47ed7f7e86
nykyforuk_vasyl <nikiforuk.vasya@gmail.com>
2025-02-19 21:28:15 +0200

    upd

--- src/redux/selectors.ts
+++ src/redux/selectors.ts
@@ -1,10 +1,11 @@
-import { Friend } from "../types/State";
-import { RootState } from "./store";
+import { Friend } from '../types/State';
+import { RootState } from './store';
 
-
-export const selectUserTokens = (state: RootState) : number => state.tokens;
+export const selectUserTokens = (state: RootState): number => state.tokens;
 export const selectUserUsername = (state: RootState) => state.username;
 export const selectUserFirstName = (state: RootState) => state.first_name;
 export const selectUserFriends = (state: RootState): Friend[] => state.friends;
 export const selectUserId = (state: RootState): number => state.id;
 export const selectUserTasks = (state: RootState) => state.usersTasks;
+export const selectIsLoading = (state: RootState) => state.loading;
+export const selectError = (state: RootState) => state.error;
