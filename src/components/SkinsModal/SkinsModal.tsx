160d123ed968e47742a85cdf87e504847238d94e
NikolayPutyata <93452811+NikolayPutyata@users.noreply.github.com>
2025-02-24 11:58:23 +0200

    upd

--- src/components/SkinsModal/SkinsModal.tsx
+++ src/components/SkinsModal/SkinsModal.tsx
@@ -7,11 +7,10 @@ const SkinsModal = () => {
   return (
     <>
       <ul className={`${s.font} flex flex-col tracking-wider gap-4`}>
-        <h2 className="text-zinc-300 my-3">Active skins 👑</h2>
 
         {/* {skins.map((skin) => ( */}
-        <li className="flex pl-2  gap-10">
-          <div className="flex flex-col justify-center w-22 h-22 overflow-hidden rounded-3xl">
+        <li className="flex pl-2  gap-8">
+          <div className="flex flex-col justify-center w-16 h-16 overflow-hidden rounded-3xl">
             <img src="/assets/shuttle-2.webp" alt="standart avatar" />
           </div>
 
