12313847dc20cb6e61375ae9312247b31483a9fd
nykyforuk_vasyl <nikiforuk.vasya@gmail.com>
2025-02-24 13:26:01 +0200

    upd BoostsModal

--- src/components/BoostsModal/BoostsModal.tsx
+++ src/components/BoostsModal/BoostsModal.tsx
@@ -1,46 +1,25 @@
 import s from '/src/App.module.css';
-// import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { selectUserBoosts } from '../../redux/selectors';
 
 const BoostsModal = () => {
-  // const boosts = useSelector(selectBoostsId);
+  const boosts = useSelector(selectUserBoosts);
 
   return (
     <>
       <ul className={`${s.font} tracking-wider flex flex-col  gap-4`}>
-      
+        {boosts.map((boost) => (
+          <li key={boost.id} className="flex pl-2  gap-8">
+            <div className="flex flex-col justify-center w-16 h-16 overflow-hidden rounded-3xl">
+              <img src={boost.boost_photo_url} alt={boost.name} />
+            </div>
 
-        {/* {boosts.map((boost) => ( */}
-        <li className="flex pl-2  gap-8">
-          <div className="flex flex-col justify-center w-16 h-16 overflow-hidden rounded-3xl">
-            <img src="/assets/engine.webp" alt="standart avatar" />
-          </div>
-
-          <div className="flex justify-start flex-col">
-            <h3 className="text-zinc-300 mb-2 break-words">Insignia</h3>
-            <p className="text-zinc-400 text-sm mb-2">X3</p>
-          </div>
-        </li>
-        {/* ))} */}
-
-        <li className="flex pl-2 gap-8">
-          <div className="flex flex-col justify-center w-16 h-16 overflow-hidden rounded-3xl">
-            <img src="/assets/commandor.webp" alt="standart avatar" />
-          </div>
-
-          <div className="flex justify-start flex-col">
-            <h3
-              className={`${s.font} text-zinc-300 mb-2 break-words tracking-wider`}
-            >
-              X-treal
-            </h3>
-            <p
-              className={`${s.font} text-zinc-400 text-sm mb-3 tracking-wider`}
-            >
-              X6
-            </p>
-          </div>
-        </li>
-
+            <div className="flex justify-start flex-col">
+              <h3 className="text-zinc-300 mb-2 break-words">{boost.name}</h3>
+              <p className="text-zinc-400 text-sm mb-2">{boost.boost_bonus}</p>
+            </div>
+          </li>
+        ))}
       </ul>
     </>
   );
