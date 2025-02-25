1a601b054101b9df996cbf4f6fd858ff45a23d1e
NikolayPutyata <93452811+NikolayPutyata@users.noreply.github.com>
2025-02-25 19:09:32 +0200

    upd

--- src/Pages/Leaderbords/SpecialItem.tsx
+++ src/Pages/Leaderbords/SpecialItem.tsx
@@ -32,8 +32,8 @@ function SpecialItem({ title, imageUrl, price, id, description, collectionId }: 
     window.Telegram.WebApp.openInvoice(invoiceLink)};
 
   return (
-    <li className="flex px-3  justify-start gap-4">
-      <div className="flex flex-col justify-center w-26 h-26 overflow-hidden rounded-3xl">
+    <li className="flex pl-3  justify-start gap-4">
+      <div className="flex flex-col justify-center w-30 h-30 overflow-hidden rounded-3xl">
         <img src={imageUrl} alt="standart avatar" />
       </div>
 
@@ -42,7 +42,7 @@ function SpecialItem({ title, imageUrl, price, id, description, collectionId }: 
          {title}
         </h3>
         <p className={`${s.font} text-zinc-400 text-[0.65rem]`}>{description}</p>
-        <div className="flex items-center gap-1 mt-1">
+        <div className="flex items-center gap-1">
 
           <p className={`${s.font} text-zinc-300 text-sm`}>{price}</p>
           <img src="/assets/telegram_star.svg" alt="telegram-star" />
