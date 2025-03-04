85eaaae95b444d3f98e3b3d6a02860f39bfc2b64
NikolayPutyata <93452811+NikolayPutyata@users.noreply.github.com>
2025-03-04 16:05:40 +0200

    upd

--- src/components/ActiveBoosts/ActiveBoosts.tsx
+++ src/components/ActiveBoosts/ActiveBoosts.tsx
@@ -19,7 +19,7 @@ const ActiveBoosts: React.FC<ActiveBoostsProps> = ({ activeBoosts }) => {
     <ul className="flex px-10 flex-col gap-2">
       {activeBoosts?.map((boost) => (
         <li key={boost.idItem} className="flex justify-between">
-          <div className="flex justify-center w-8 h-8 overflow-hidden rounded-3xl">
+          <div className="flex justify-center w-12 h-12 overflow-hidden rounded-3xl">
             <img src={boost.boost_photo_url} alt={boost.name} />
           </div>
 
