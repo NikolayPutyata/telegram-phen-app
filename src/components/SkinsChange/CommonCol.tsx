03bbbebbd7263c9a75d5ae77269abd3a240848a8
NikolayPutyata <93452811+NikolayPutyata@users.noreply.github.com>
2025-03-12 12:23:17 +0200

    upd

--- src/components/SkinsChange/CommonCol.tsx
+++ src/components/SkinsChange/CommonCol.tsx
@@ -6,11 +6,11 @@ const CommonCol = () => {
       
 
       <div className="px-8 pt-5 pb-5 bg-neutral-900 rounded-3xl mx-4 grid grid-cols-2 gap-4">
-  <div className='bg-red-700 col-span-2 rounded-3xl border-1 border-gray-400 overflow-hidden h-44'><img src="https://res.cloudinary.com/dv1acgeyp/image/upload/v1740682767/shut_11zon_g5onzi.webp" alt="" className='object-cover w-full h-full'/></div>
-  <div className='bg-red-300 rounded-3xl border-1 border-gray-400 overflow-hidden'><img src="https://res.cloudinary.com/dv1acgeyp/image/upload/v1741774353/Group_67_5_tglkil.png" alt="" className='object-cover w-full h-full'/></div>
+  <div className='bg-red-700 col-span-2 rounded-3xl border-1 border-gray-400 overflow-hidden h-44'><img src="https://res.cloudinary.com/dv1acgeyp/image/upload/v1741774739/Group_67_7_oehn0k.png" alt="" className='object-cover w-full h-full'/></div>
+  <div className='bg-red-300 rounded-3xl border-1 border-gray-400 overflow-hidden'><img src="https://res.cloudinary.com/dv1acgeyp/image/upload/v1741774739/Group_67_7_oehn0k.png" alt="" className='object-cover w-full h-full'/></div>
   <div className='grid grid-cols-2 gap-2 rounded-3xl'>
     <div className='bg-red-50 rounded-3xl text-black border-1 border-gray-400  overflow-hidden'><img src="https://res.cloudinary.com/dv1acgeyp/image/upload/v1741770392/basic_comandor_11zon_viuhlg.webp" alt="" className='object-cover w-full h-full'/></div>
-    <div className='bg-red-50 rounded-3xl text-black border-1 border-gray-400 overflow-hidden'><img src="https://res.cloudinary.com/dv1acgeyp/image/upload/v1741774418/Group_67_6_uefm0l.png" alt="" className='object-cover w-full h-full'/></div>
+    <div className='bg-red-50 rounded-3xl text-black border-1 border-gray-400 overflow-hidden'><img src="https://res.cloudinary.com/dv1acgeyp/image/upload/v1741770392/planet_2_9_11zon_i52kfr.webp" alt="" className='object-cover w-full h-full'/></div>
         </div>
         <div className={`${s.font} flex justify-center text-lg gap-2`}> <span className='flex justify-center items-center gap-1'><div className='flex items-center justify-center'>420</div><img
                 src="/assets/Group 61.png"
