6d5deb5aad59bb8d90c713ad1ae97a52c8d21af3
nykyforuk_vasyl <nikiforuk.vasya@gmail.com>
2025-03-18 16:06:23 +0200

    SilverCol change

--- src/components/SkinsChange/SilverCol.tsx
+++ src/components/SkinsChange/SilverCol.tsx
@@ -1,59 +1,21 @@
-import s from '/src/App.module.css';
+import SkinsComponent from '../SkinsComponent/SkinsComponent';
+
+const data = {
+  imgStarFlight:
+    'https://res.cloudinary.com/dv1acgeyp/image/upload/v1741783041/sht3_11zon_jlbihy.webp',
+  imgShip:
+    'https://res.cloudinary.com/dv1acgeyp/image/upload/v1742288006/silverBase_11zon_jdybwi.webp',
+  imgComandor:
+    'https://res.cloudinary.com/dv1acgeyp/image/upload/v1741783042/comS_11zon_d4gn94.webp',
+  planet:
+    'https://res.cloudinary.com/dv1acgeyp/image/upload/v1741783044/planet3_11zon_kbdrpe.webp',
+  price: '420',
+  plus: '+ 10%',
+  styleBorder: 'border-[#C0C0C0]',
+};
 
 const SilverCol = () => {
-  return (
-    <div className="rounded-3xl mx-4 grid grid-cols-2 gap-4">
-      <div className="col-span-2 rounded-3xl border-1 border-[#C0C0C0] overflow-hidden h-44">
-        <img
-          src="https://res.cloudinary.com/dv1acgeyp/image/upload/v1741783041/sht3_11zon_jlbihy.webp"
-          alt=""
-          className="object-cover w-full h-full"
-        />
-      </div>
-      <div className="rounded-3xl border-1 border-[#C0C0C0] overflow-hidden">
-        <img
-          src="https://res.cloudinary.com/dv1acgeyp/image/upload/v1742288006/silverBase_11zon_jdybwi.webp"
-          alt=""
-          className="object-cover w-full h-full"
-        />
-      </div>
-      <div className="grid grid-cols-2 gap-2 rounded-3xl">
-        <div className="bg-red-50 rounded-3xl text-black border-1 border-[#C0C0C0] overflow-hidden">
-          <img
-            src="https://res.cloudinary.com/dv1acgeyp/image/upload/v1741783042/comS_11zon_d4gn94.webp"
-            alt=""
-            className="object-cover w-full h-full"
-          />
-        </div>
-        <div className="bg-red-50 rounded-3xl text-black border-1 border-[#C0C0C0] overflow-hidden">
-          <img
-            src="https://res.cloudinary.com/dv1acgeyp/image/upload/v1741783044/planet3_11zon_kbdrpe.webp"
-            alt=""
-            className="object-cover w-full h-full"
-          />
-        </div>
-      </div>
-      <div className={`${s.font} flex justify-center text-lg gap-3`}>
-        <span className="flex justify-center items-center gap-1">
-          <div className="flex items-center justify-center">420</div>
-          <img
-            src="/assets/Group61.png"
-            alt="Phenerium"
-            width="20px"
-            height="25px"
-          />
-        </span>
-        <div className="flex items-center justify-center text-gray-400 text-sm">
-          + 10%
-        </div>
-      </div>
-      <div className="flex justify-center">
-        <button className="btn btn-primary w-full rounded-4xl self-center bg-gradient-to-r from-blue-500 to-purple-500">
-          Claim
-        </button>
-      </div>
-    </div>
-  );
+  return <SkinsComponent {...data} />;
 };
 
 export default SilverCol;
