f9530ed180923f9ea383459d3ba37a9b6b42156e
NikolayPutyata <93452811+NikolayPutyata@users.noreply.github.com>
2025-03-18 13:08:23 +0200

    upd

--- src/Pages/Leaderbords/SkinsItem.tsx
+++ src/Pages/Leaderbords/SkinsItem.tsx
@@ -20,11 +20,7 @@ function SkinsItem({ imageUrlSmall, price, bonus, name }: SkinsItemProps) {
         <p className={`${s.font} text-zinc-400 text-xs mt-1`}>{bonus}</p>
         <div className="flex items-center gap-1.5 mt-1">
           <p className={`${s.font} text-zinc-300 text-sm`}>{price}</p>
-          <img
-            src="/public/assets/Group61.png"
-            alt="telegram-star"
-            width={16}
-          />
+          <img src="/assets/Group61.png" alt="telegram-star" width={16} />
         </div>
 
         <button className="btn btn-primary w-24 h-8 rounded-4xl mt-1 bg-gradient-to-r from-blue-500 to-purple-500">
