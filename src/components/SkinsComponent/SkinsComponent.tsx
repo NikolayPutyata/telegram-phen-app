d8b8c496af0312a4d4dfa2b7f44bd5694f99ad60
nykyforuk_vasyl <nikiforuk.vasya@gmail.com>
2025-03-18 15:40:01 +0200

    GoldCol change

--- src/components/SkinsComponent/SkinsComponent.tsx
+++ src/components/SkinsComponent/SkinsComponent.tsx
@@ -5,6 +5,9 @@ type SkinsData = {
   imgCor: string;
   imgCom: string;
   planet: string;
+  price: string;
+  plus: string;
+  styleBorder: string;
 };
 
 const SkinsComponent: React.FC<SkinsData> = ({
@@ -12,28 +15,37 @@ const SkinsComponent: React.FC<SkinsData> = ({
   imgCom,
   imgCor,
   planet,
+  price,
+  plus,
+  styleBorder,
 }) => {
   return (
     <div className=" rounded-3xl mx-4 grid grid-cols-2 gap-4">
-      <div className="col-span-2 rounded-3xl border-1 border-amber-400 overflow-hidden h-44">
+      <div
+        className={`${styleBorder} col-span-2 rounded-3xl border-1 overflow-hidden h-44`}
+      >
         <img src={imgZor} alt="" className="object-cover w-full h-full" />
       </div>
-      <div className="rounded-3xl border-1 border-amber-400 overflow-hidden">
+      <div className={`${styleBorder} rounded-3xl border-1 overflow-hidden`}>
         <img src={imgCor} alt="" className="object-cover w-full h-full" />
       </div>
 
       <div className="grid grid-cols-2 gap-2 rounded-3xl">
-        <div className="bg-red-50 rounded-3xl  border-1 border-amber-400  overflow-hidden">
+        <div
+          className={`${styleBorder} bg-red-50 rounded-3xl  border-1  overflow-hidden`}
+        >
           <img src={imgCom} alt="" className="object-cover w-full h-full" />
         </div>
-        <div className="bg-red-50 rounded-3xl  border-1 border-amber-400 overflow-hidden">
+        <div
+          className={`${styleBorder} bg-red-50 rounded-3xl  border-1 overflow-hidden`}
+        >
           <img src={planet} alt="" className="object-cover w-full h-full" />
         </div>
       </div>
 
       <div className={`${s.font} flex justify-center text-lg gap-3`}>
         <span className="flex justify-center items-center gap-1">
-          <div className="flex items-center justify-center">420</div>
+          <div className="flex items-center justify-center">{price}</div>
           <img
             src="/assets/Group61.png"
             alt="Phenerium"
@@ -43,7 +55,7 @@ const SkinsComponent: React.FC<SkinsData> = ({
         </span>
 
         <div className="flex items-center justify-center text-gray-400 text-sm">
-          + 20%
+          {plus}
         </div>
       </div>
 
