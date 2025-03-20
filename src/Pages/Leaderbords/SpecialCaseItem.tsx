478131d570c1d849262c14a9f8015c5845cacfcb
NikolayPutyata <93452811+NikolayPutyata@users.noreply.github.com>
2025-03-20 19:21:30 +0200

    upd

--- src/Pages/Leaderbords/SpecialCaseItem.tsx
+++ src/Pages/Leaderbords/SpecialCaseItem.tsx
@@ -1,8 +1,9 @@
-
 import { useSelector } from 'react-redux';
 import { createStarInvoice } from '../../utils/createStarInvoice';
 import s from '/src/App.module.css';
 import { selectUserId } from '../../redux/selectors';
+import { ClipLoader } from 'react-spinners';
+import { useState } from 'react';
 
 interface SpecialItemProps {
   id: number;
@@ -13,53 +14,61 @@ interface SpecialItemProps {
   collectionId: number;
 }
 
-function SpecialItem({ title, imageUrl, price, id, description, collectionId }: SpecialItemProps) {
-
+function SpecialItem({
+  title,
+  imageUrl,
+  price,
+  id,
+  description,
+  collectionId,
+}: SpecialItemProps) {
   const userId = useSelector(selectUserId);
-
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleBuyClick = async () => {
+    setIsLoading(true);
 
     const invoiceLink = await createStarInvoice({
       title: title,
       description: description,
       prices: [{ label: 'Price', amount: Number(price) }],
-      currency: "XTR",
+      currency: 'XTR',
       provider_token: '',
       payload: `ORDER_${userId}_${collectionId}_${id}`,
     });
 
     window.Telegram.WebApp.openInvoice(invoiceLink, (status) => {
       if (status === 'paid') {
-        alert('Спасибо за покупку!');
+        setIsLoading(false);
       } else if (status === 'cancelled') {
-        alert('Оплата отменена');
+        setIsLoading(false);
       } else if (status === 'failed') {
-        alert('Ошибка оплаты');
+        setIsLoading(false);
       }
     });
-
-   
-  
-};
+  };
 
   return (
     <li className="flex pl-3  justify-start gap-4">
       <div className="flex flex-col justify-center w-30 h-30 overflow-hidden rounded-3xl">
-        <img src={imageUrl} alt="standart avatar"/>
+        <img src={imageUrl} alt="standart avatar" />
       </div>
 
       <div className="flex flex-col gap-1">
-        <h3 className={`${s.font} text-zinc-300  break-words`}>
-         {title}
-        </h3>
-        <p className={`${s.font} text-zinc-400 text-[0.65rem]`}>{description}</p>
+        <h3 className={`${s.font} text-zinc-300  break-words`}>{title}</h3>
+        <p className={`${s.font} text-zinc-400 text-[0.65rem]`}>
+          {description}
+        </p>
         <div className="flex items-center gap-1">
-
           <p className={`${s.font} text-zinc-300 text-sm`}>{price}</p>
           <img src="/assets/telegram_star.svg" alt="telegram-star" />
         </div>
-        <button className="btn btn-primary w-24 h-8 rounded-4xl mt-1 bg-gradient-to-r from-blue-500 to-purple-500" onClick={handleBuyClick}>Buy</button>
+        <button
+          className="btn btn-primary w-24 h-8 rounded-4xl mt-1 bg-gradient-to-r from-blue-500 to-purple-500"
+          onClick={handleBuyClick}
+        >
+          {isLoading ? <ClipLoader size={17} color={'#ededed'} /> : 'Buy'}
+        </button>
       </div>
     </li>
   );
