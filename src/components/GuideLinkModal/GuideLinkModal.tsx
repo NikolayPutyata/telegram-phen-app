76908999b8a2aaadd9892b0f245f5abd9072f5ef
nykyforuk_vasyl <nikiforuk.vasya@gmail.com>
2025-03-21 21:46:59 +0200

    upd GaideLinkModal

--- src/components/GuideLinkModal/GuideLinkModal.tsx
+++ src/components/GuideLinkModal/GuideLinkModal.tsx
@@ -6,10 +6,23 @@ const GaideLinkModal = () => {
       className={`${s.font} list-decimal flex flex-col gap-8 list-inside text-lg text-zinc-300 tracking-wider`}
     >
       <li>
-        Go to Phenerium
+        Open Phenerium
         <div className="flex flex-col gap-4 px-4">
           <p className="text-sm text-zinc-400">
-            Open Phenerium, go to the information menu.
+            Open Phenerium, and go to the info menu.
+          </p>
+          <img
+            src="/assets/language/photo_y.jpg"
+            alt=""
+            className="object-cover w-full h-full"
+          />
+        </div>
+      </li>
+      <li className="">
+        Find Affiliate Program
+        <div className="flex flex-col gap-4 px-4">
+          <p className="text-sm text-zinc-400">
+            Scroll to "Affiliate Program" and select one.
           </p>
           <img
             src="https://res.cloudinary.com/dv1acgeyp/image/upload/v1739261781/friends_yda58q.webp"
@@ -19,11 +32,10 @@ const GaideLinkModal = () => {
         </div>
       </li>
       <li className="">
-        Find an affiliate program
+        Copy Link
         <div className="flex flex-col gap-4 px-4">
           <p className="text-sm text-zinc-400">
-            Scroll down to the "Affiliate Program" section, click on an
-            available affiliate program to open its details.
+            Locate the referral link and copy it.
           </p>
           <img
             src="https://res.cloudinary.com/dv1acgeyp/image/upload/v1739261781/friends_yda58q.webp"
@@ -33,10 +45,10 @@ const GaideLinkModal = () => {
         </div>
       </li>
       <li className="">
-        Copy the link
+        Go to "Friends"
         <div className="flex flex-col gap-4 px-4">
           <p className="text-sm text-zinc-400">
-            Find the referral link, click "Copy" or hold to copy.
+            Return to Phenerium and go to the 'Friends' section.
           </p>
           <img
             src="https://res.cloudinary.com/dv1acgeyp/image/upload/v1739261781/friends_yda58q.webp"
@@ -45,12 +57,12 @@ const GaideLinkModal = () => {
           />
         </div>
       </li>
+
       <li className="">
-        Go to the "Friends" tab
+        Paste and Done
         <div className="flex flex-col gap-4 px-4">
           <p className="text-sm text-zinc-400">
-            In Phenerium, click the "Play" button to activate the next step,
-            select the "Friends" section.
+            Paste the link into the field and press "Done".
           </p>
           <img
             src="https://res.cloudinary.com/dv1acgeyp/image/upload/v1739261781/friends_yda58q.webp"
@@ -59,10 +71,13 @@ const GaideLinkModal = () => {
           />
         </div>
       </li>
+
       <li className="">
-        Paste the link and send
+        Get Bonus
         <div className="flex flex-col gap-4 px-4">
-          <p className="text-sm text-zinc-400">Go to Telegram settings</p>
+          <p className="text-sm text-zinc-400">
+            Bonus will be credited automatically after submission.
+          </p>
           <img
             src="https://res.cloudinary.com/dv1acgeyp/image/upload/v1739261781/friends_yda58q.webp"
             alt=""
