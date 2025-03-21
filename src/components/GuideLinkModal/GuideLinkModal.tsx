c680122184ea07e6183460895d23e0e9a41dc608
nykyforuk_vasyl <nikiforuk.vasya@gmail.com>
2025-03-21 20:11:15 +0200

    upd GaideLinkModal

--- src/components/GuideLinkModal/GuideLinkModal.tsx
+++ src/components/GuideLinkModal/GuideLinkModal.tsx
@@ -1,8 +1,66 @@
+import s from '/src/App.module.css';
+
 const GaideLinkModal = () => {
   return (
-    <div>
-      <h1>GuideLinkModal</h1>
-    </div>
+    <ol
+      className={`${s.font} list-decimal flex flex-col gap-8 list-inside text-xl text-zinc-300 tracking-wider`}
+    >
+      <li>
+        Step one
+        <div className="flex flex-col gap-4 px-4">
+          <p className="text-base text-zinc-400">Go to Telegram settings</p>
+          <img
+            src="https://res.cloudinary.com/dv1acgeyp/image/upload/v1739261781/friends_yda58q.webp"
+            alt=""
+            className="object-cover w-full h-full"
+          />
+        </div>
+      </li>
+      <li className="">
+        Step two
+        <div className="flex flex-col gap-4 px-4">
+          <p className="text-base text-zinc-400">Go to Telegram settings</p>
+          <img
+            src="https://res.cloudinary.com/dv1acgeyp/image/upload/v1739261781/friends_yda58q.webp"
+            alt=""
+            className="object-cover w-full h-full"
+          />
+        </div>
+      </li>
+      <li className="">
+        Step three
+        <div className="flex flex-col gap-4 px-4">
+          <p className="text-base text-zinc-400">Go to Telegram settings</p>
+          <img
+            src="https://res.cloudinary.com/dv1acgeyp/image/upload/v1739261781/friends_yda58q.webp"
+            alt=""
+            className="object-cover w-full h-full"
+          />
+        </div>
+      </li>
+      <li className="">
+        Step four
+        <div className="flex flex-col gap-4 px-4">
+          <p className="text-base text-zinc-400">Go to Telegram settings</p>
+          <img
+            src="https://res.cloudinary.com/dv1acgeyp/image/upload/v1739261781/friends_yda58q.webp"
+            alt=""
+            className="object-cover w-full h-full"
+          />
+        </div>
+      </li>
+      <li className="">
+        Step five
+        <div className="flex flex-col gap-4 px-4">
+          <p className="text-base text-zinc-400">Go to Telegram settings</p>
+          <img
+            src="https://res.cloudinary.com/dv1acgeyp/image/upload/v1739261781/friends_yda58q.webp"
+            alt=""
+            className="object-cover w-full h-full"
+          />
+        </div>
+      </li>
+    </ol>
   );
 };
 
